621d4292de4e3598997a55c00a3ca043028c29ee
pranav rama <pranavrama9999@utexas.edu>
2022-07-04 11:06:43 -0400

    update example to use wide timer

--- examples/generic_timer.rs
+++ examples/generic_timer.rs
@@ -87,8 +87,8 @@ fn main() -> ! {
 
     //  let mut utp_adc = generic_adc_unit::new(tm4c_adc, pe3, pe2, pe1, pe0, pe5, pe4);
   
- 	let mut tm4c_timer0 = Timer::<tm4c123x::TIMER0>::timer0(p.TIMER0, MillisU16(Millis(4)), &sc.power_control, &clocks);
- 	let mut tm4c_timer1 = Timer::<tm4c123x::TIMER1>::timer1(p.TIMER1, MillisU16(Millis(4)), &sc.power_control, &clocks);
+ 	let mut tm4c_timer0 = Timer::<tm4c123x::WTIMER0>::wtimer0(p.WTIMER0, MillisU16(Millis(54000)), &sc.power_control, &clocks);
+ 	let mut tm4c_timer1 = Timer::<tm4c123x::WTIMER1>::wtimer1(p.WTIMER1, MillisU16(Millis(54000)), &sc.power_control, &clocks);
 
  	let mut utp_timer = generic_timer_unit::<MillisU16, _, _, _>::new(tm4c_timer0, tm4c_timer1);
 
@@ -96,7 +96,7 @@ fn main() -> ! {
     utp_timer.set_state(TimerId::T0, TimerState::WithPeriod(core::num::NonZeroU16::new(4000).unwrap()));
 
     //utp_timer.set_mode(TimerId::T1, TimerMode::Repeated);
-    utp_timer.set_state(TimerId::T1, TimerState::WithPeriod(core::num::NonZeroU16::new(6000).unwrap()));
+    utp_timer.set_state(TimerId::T1, TimerState::WithPeriod(core::num::NonZeroU16::new(65535).unwrap()));
 
     //  utp_adc.set_state(AdcPin::A0, AdcState::Enabled);
     //  utp_adc.set_state(AdcPin::A1, AdcState::Enabled);
